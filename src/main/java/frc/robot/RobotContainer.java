81116d2a446c5ac206eed2f24df74894b47b67e8
N0Hbdyy <overmail1551@gmail.com>
2021-02-10 19:18:53 -0600

    new balltower Logic v2

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -122,7 +122,7 @@ public class RobotContainer {
 
     btn5.whenPressed(new InstantCommand(m_Transport::resetBallCounter));
 
-    //testing out trigger for balltower
+    //testing out trigger for ballTower
     new Trigger(
             () -> {
               return m_Transport.getLow();
